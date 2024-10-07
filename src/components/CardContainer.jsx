36f8e82799635fac35a95156cd2d6dcc3f3dd836
S. AASIM'AHMED W. <aasimahmedsiddiqui1@gmail.com>
2024-10-07 22:48:57 +0530

    add: battery card

---
+++ src/components/CardContainer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CardContainer() {
+  return (
+    <div>
+      <div className="gradient-cards">
+        {/* <Location />
+        <Battery /> */}
+      </div>
+    </div>
+  );
+}
+
+export default CardContainer;
