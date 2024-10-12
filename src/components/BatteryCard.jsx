94ef95a9e7e6a13bf7b2d7bb7e660edf4d209468
S. AASIM'AHMED W. <aasimahmedsiddiqui1@gmail.com>
2024-10-12 18:03:07 +0530

    fix battery

--- src/components/BatteryCard.jsx
+++ src/components/BatteryCard.jsx
@@ -23,39 +23,46 @@ function BatteryCard() {
   // Update battery info in Firestore
   const updateBatteryInfoInFirestore = (batteryData) =>
     userID &&
-    updateDoc(doc(db, "BOTS", userID), {
-      "BATTERY.PERCENTAGE": batteryData.level,
-      "BATTERY.TEMPERATURE": batteryData.temperature,
-      "BATTERY.IS_CHARGING": batteryData.charging,
-    }, { merge: true }).catch(console.error);
+    updateDoc(
+      doc(db, "BOTS", userID),
+      {
+        "BATTERY.PERCENTAGE": batteryData.level,
+        "BATTERY.TEMPERATURE": batteryData.temperature,
+        "BATTERY.IS_CHARGING": batteryData.charging,
+      },
+      { merge: true }
+    ).catch(console.error);
 
   // Get battery information and listen for changes
   const getBatteryInfo = () => {
-    let prevCharging = null;
-    let prevLevel = null;
-    navigator.getBattery().then((battery) => {
-      const updateBatteryLevel = () => {
-        if (
-          prevCharging !== battery.charging ||
-          prevLevel !== battery.level
-        ) {
-          const updatedInfo = {
-            level: Math.round(battery.level * 100),
-            charging: battery.charging,
-            temperature: Math.floor(Math.random() * (40 - 20 + 1)) + 20, // Simulated temperature
-          };
-          setBatteryInfo(updatedInfo);
-          updateBatteryInfoInFirestore(updatedInfo); // Update Firestore with the new battery data
-          prevCharging = battery.charging;
-          prevLevel = battery.level;
-        }
+    let prevInfo = null;
+    const updateBatteryLevel = (battery) => {
+      const { charging, level } = battery;
+      const updatedInfo = {
+        level: Math.round(level * 100),
+        charging,
+        temperature: Math.floor(Math.random() * (40 - 20 + 1)) + 20, // Simulated temperature
       };
 
-      battery.addEventListener("levelchange", updateBatteryLevel);
-      battery.addEventListener("chargingchange", updateBatteryLevel);
+      if (
+        prevInfo === null ||
+        prevInfo.level !== updatedInfo.level ||
+        prevInfo.charging !== updatedInfo.charging
+      ) {
+        setBatteryInfo(updatedInfo);
+        updateBatteryInfoInFirestore(updatedInfo); // Update Firestore with the new battery data
+        prevInfo = updatedInfo;
+      }
+    };
 
-      // Initial setting of battery info
-      updateBatteryLevel();
+    navigator.getBattery().then((battery) => {
+      updateBatteryLevel(battery);
+      battery.addEventListener("levelchange", () =>
+        updateBatteryLevel(battery)
+      );
+      battery.addEventListener("chargingchange", () =>
+        updateBatteryLevel(battery)
+      );
     });
   };
 
